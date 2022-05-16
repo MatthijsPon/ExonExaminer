84b0dd5802b4ec694c19a52b32b35607ce3566a9
Matthijs Pon <matthijs.pon@outlook.com>
2022-05-16 09:43:38 +0200

    Implementation of scatterplot based on ratio and moving functions to helpers.py

--- transcript_expression.py
+++ transcript_expression.py
@@ -12,7 +12,7 @@ import gzip
 import statistics as stats
 from matplotlib import pyplot as plt
 
-from parse import yield_gff_line
+from helpers import get_internal_exons, yield_single_gene, determine_target_exon
 
 
 def parse_arg():
@@ -36,84 +36,6 @@ def parse_arg():
     return args.gff3_file, args.expression_file, args.expression_companion, args.out, args.complexity
 
 
-def determine_gff(gff_8, identifier, further_split=None):
-    """Select a specific object from the information column (column 8 in 0-count)
-
-    :param gff_8: list of str, information column, split
-    :param identifier: str, identifier of column of interest (e.g. ID=). Everything
-                       identifier will be removed from return string
-    :param further_split: str, seperator on which the column should be split on
-                          after removal of the identifier (default: None)
-    :return: str, information following the identifier, otherwise None
-    """
-    for item in gff_8.split(";"):
-        if item.startswith(identifier):
-            if further_split:
-                return item[len(identifier):].split(further_split)
-            return item[len(identifier):]
-    return None
-
-
-def yield_single_gene(file_object):
-    """Yield a single gene from a gff3 file, including related protein coding transcripts and exons.
-
-    :param file_object: open file object, gff3 file
-    :return: pandas dataframe, gene information containing type, id, parent, size and alias columns
-    """
-    gene_dict = {
-        "type": [],
-        "id": [],
-        "parent": [],
-        "size": [],
-    }
-
-    # A dict needs all three to be complete
-    gene_id = ""
-    transcript = []
-    exon = []
-
-    for line in yield_gff_line(file_object):
-        if line[2] == "gene":
-            if gene_id and transcript and exon:
-                yield pd.DataFrame.from_dict(gene_dict)
-            # Reset stats
-            gene_dict = {
-                "type": [],
-                "id": [],
-                "parent": [],
-                "size": [],
-            }
-            gene_id = determine_gff(line[8], "ID=gene:")
-            transcript = []
-            exon = []
-            # Add data to dict
-            gene_dict["type"].append("gene")
-            gene_dict["id"].append(gene_id)
-            gene_dict["parent"].append(None)
-            gene_dict["size"].append(None)
-        elif line[2] in ["transcript", "mRNA", "primary_transcript"] and \
-                determine_gff(line[8], "biotype=") == "protein_coding":
-            if determine_gff(line[8], "Parent=gene:") == gene_id:
-                trans_id = determine_gff(line[8], "ID=transcript:")
-                transcript.append(trans_id)
-                # Add data to dict
-                gene_dict["type"].append("transcript")
-                gene_dict["id"].append(trans_id)
-                gene_dict["parent"].append(gene_id)
-                gene_dict["size"].append(None)
-        elif line[2] == "exon":
-            if determine_gff(line[8], "Parent=transcript:") in transcript:
-                exon_id = determine_gff(line[8], "Name=")
-                exon.append(exon_id)
-                gene_dict["type"].append("exon")
-                gene_dict["id"].append(exon_id)
-                gene_dict["parent"].append(determine_gff(line[8], "Parent=transcript:"))
-                gene_dict["size"].append((int(line[4]) - int(line[3])))
-
-    if gene_id and transcript and exon:
-        yield pd.DataFrame.from_dict(gene_dict)
-
-
 def create_averaged_expression(exp_file, exp_comp, out_dir, complexity=1):
     """Create an expression file with averaged expression over complexity 1 or 2
 
@@ -132,8 +54,6 @@ def create_averaged_expression(exp_file, exp_comp, out_dir, complexity=1):
     # Subset df to only relevant columns
     companion_df = companion_df[["SAMPID", column_of_interest]]
     options = tuple([item for item in companion_df[column_of_interest].unique() if item != "Bone Marrow"])
-    with open("out.txt", "w+") as f:
-        f.write(", ".join(list(companion_df["SAMPID"])))
     option_columns = {x: [] for x in options}
     not_measured = set()
     # Open gzipped file and file to output data to
@@ -220,37 +140,6 @@ def acquire_expression_dataframe(exp_file, exp_comp, out_dir, complexity=0):
     return pd.read_csv(save_location, header=0, sep="\t")
 
 
-def determine_target_exon(gene_df, target):
-    """Determine which exon to target
-
-    :param gene_df: pandas dataframe, gene dataframe
-    :param target: str, "largest" or "largest_internal" to decide target
-    :return: str, geneID if found, otherwise None
-    """
-    if target == "largest":
-        return gene_df[gene_df["size"] == gene_df["size"].max()]["id"].iloc[0]  # Select largest exon
-    elif target == "largest_internal":
-        non_prime = set()
-        only_exons = gene_df[gene_df["type"] == "exon"]
-        # Select exons per transcript and apply True only if they are prime
-        for idx, group in only_exons.groupby("parent"):
-            exons_transcript = list(group["id"])
-            exons_transcript.pop(0)  # Remove the first exon
-            if len(exons_transcript) >= 1:  # Cannot pop twice if there is only one exon
-                exons_transcript.pop(-1)  # Remove the last exon
-            # All exons that have a non-prime function at least once are possible targets
-            for exon in exons_transcript:
-                non_prime.add(exon)
-        if len(non_prime) >= 1:  # If there are internal exons return the largest, otherwise return none.
-            subset_internal = gene_df[gene_df["id"].isin(non_prime)]  # Select only internal exons from gene_df
-            # Select largest exon from the subset
-            largest = subset_internal[subset_internal["size"] == subset_internal["size"].max()]["id"].iloc[0]
-            return largest
-        return None
-    else:
-        raise ValueError("Incorrect target given in determine_target_exon().")
-
-
 def ratio_expression_exon(gene_df, expression_df, target_exon=None):
     """Calculate the ratio between expression of transcripts including target exon and all transcripts.
 
@@ -280,12 +169,12 @@ def ratio_expression_exon(gene_df, expression_df, target_exon=None):
     if len(total_exp_all) == 0:  # If the gene is not measured, discard the ratio
         return None
     total_all = stats.fmean(list(total_exp_all))
-    if len(total_exp_target) == 0:  # Target transcript was not measured, skip
-        return None
-    total_target = stats.fmean(list(total_exp_target))
-
     if total_all == 0.0:  # If there is no expression measured at all, discard the ratio
         return None
+    if len(total_exp_target) == 0:  # Target transcript was not measured, return None
+        return None
+
+    total_target = stats.fmean(list(total_exp_target))
     ratio = total_target / total_all
     return ratio
 
@@ -299,30 +188,9 @@ def create_full_scatterplot(data, out_dir, complexity):
     :return: None, figures are created and written to disk
     """
     plt.rcParams.update({"font.size": 20, "figure.figsize": (30, 20)})
-    data.plot(x="max_exon", y="ratio", style="o")
+    data.plot(x="size", y="ratio", style="o")
     plt.savefig("{}full_scatterplot_complexity{}.png".format(out_dir, complexity))
     plt.close()
-
-
-def create_scatterplot_no_transcripts(data, out_dir, complexity):
-    """Create a scatterplot split on No. of transcripts in gene
-
-    :param data: pandas dataframe, containing data to plot
-    :param out_dir: str, output directory
-    :param complexity: int, complexity used for data creation
-    :return: None, figures are created and written to disk
-    """
-    trans_numbers = data["no_trans"].unique()
-    trans_numbers.sort()
-    closest_root = math.ceil(math.sqrt(len(trans_numbers)))  # Determine minimum square size needed to plot all graphs
-    # Plot stuff
-    plt.rcParams.update({"font.size": 20, "figure.figsize": (30, 20)})
-    # Plot each image separately
-    for key in trans_numbers:
-        data.loc[data["no_trans"] == key].plot(x="max_exon", y="ratio", style="o",
-                                               title="{} transcripts in gene".format(key))
-        plt.savefig("{}scatterplot_complexity{}_{}trans.png".format(out_dir, complexity, key))
-        plt.close()
 
 
 def create_zoom_scatterplot(data, out_dir, complexity):
@@ -334,40 +202,73 @@ def create_zoom_scatterplot(data, out_dir, complexity):
     :return: None, figures are created and written to disk
     """
     plt.rcParams.update({"font.size": 20, "figure.figsize": (30, 20)})
-    data = data.loc[(data["max_exon"] >= 500) & (data["max_exon"] <= 10000)]  # Select certain sizes
+    data = data.loc[(data["size"] >= 500) & (data["size"] <= 10000)]  # Select certain sizes
     data = data.loc[data["ratio"] <= 4]
-    data.plot(x="max_exon", y="ratio", style="o")
+    data.plot(x="size", y="ratio", style="o")
     plt.savefig("{}full_zoom_scatterplot_complexity{}.png".format(out_dir, complexity))
+    plt.close()
+
+
+def color_scatterplot_exonincorporation(data, out_dir, bigger_genes, smaller_genes):
+    """"""
+    data_high = data.loc[data["id"].isin(bigger_genes)].loc[data["size"] < 5000]
+    data_low = data.loc[data["id"].isin(smaller_genes)].loc[data["size"] < 5000]
+    plt.rcParams.update({"figure.figsize": (19.2, 16.8)})
+    fig, ax = plt.subplots()
+    ax.scatter(x=data_low["size"], y=data_low["ratio"], s=3, c="Blue", label="Exon usage < 0")
+    ax.scatter(x=data_high["size"], y=data_high["ratio"], s=3,  c="Yellow", label="Exon usage >= 0")
+    plt.xlim(right=5000)
+    plt.ylim(top=3)
+    ax.legend()
+    plt.savefig("{}/scatterplot_split_on_usage.png".format(out_dir))
     plt.close()
 
 
 def main():
     """Main function"""
-    start_time = time.perf_counter()
     # Parse arguments
     gff, exp, exp_comp, out_dir, complexity = parse_arg()
-    # Get expression dataframe
-    expression_df = acquire_expression_dataframe(exp, exp_comp, out_dir, complexity)
-    data = {
-        "no_trans": [],
-        "max_exon": [],
-        "ratio": []
-    }
-    with open(gff) as file:
-        for gene_df in yield_single_gene(file):
-            # For each gene determine the expression ratio
-            ratio = ratio_expression_exon(gene_df, expression_df)
-            if ratio:
-                data["no_trans"].append((gene_df.type.values == "transcript").sum())  # Number of transcripts in gene
-                data["max_exon"].append(gene_df["size"].max())  # Size of the largest exon
-                data["ratio"].append(ratio)  # Ratio expression largest exon / total expression
-            else:  # If there is no ratio, skip the gene
-                continue
-    data = pd.DataFrame(data)
+    if not os.path.exists("./data/temp/expression.pickle"):
+        # Get expression dataframe
+        expression_df = acquire_expression_dataframe(exp, exp_comp, out_dir, complexity)
+        data = {
+            "id": [],
+            "size": [],
+            "ratio": []
+        }
+        with open(gff) as file:
+            for gene_df in yield_single_gene(file):
+                # For each gene determine the expression ratio
+                internal_exons = get_internal_exons(gene_df)
+                if internal_exons is None:
+                    continue  # If there are no internal exons, skip the gene
+                for exon in internal_exons:
+                    ratio = ratio_expression_exon(gene_df, expression_df, target_exon=exon)
+                    if ratio:
+                        data["id"].append(exon)  # Number of transcripts in gene
+                        data["size"].append(gene_df.loc[gene_df["id"] == exon, "size"].iloc[0])
+                        data["ratio"].append(ratio)  # Ratio expression largest exon / total expression
+                    else:  # If there is no ratio, skip the exon
+                        continue
+        data = pd.DataFrame(data)
+        data.to_pickle("./data/temp/expression.pickle")
+    else:
+        data = pd.read_pickle("./data/temp/expression.pickle")
+
     create_full_scatterplot(data, out_dir, complexity)
     create_zoom_scatterplot(data, out_dir, complexity)
-    create_scatterplot_no_transcripts(data, out_dir, complexity)
-    print("Done, time taken: {:.2f} seconds\n".format(time.perf_counter() - start_time))
+
+    # Create scatterplot coloured on exon incorporation
+    if os.path.exists("./data/out/transcript_ratio/genes_ratio_biggerequal_0.csv") and \
+            os.path.exists("./data/out/transcript_ratio/genes_ratio_smaller_0.csv"):
+        with open("./data/out/transcript_ratio/genes_ratio_biggerequal_0.csv") as file:
+            bigger_genes = file.read().strip().split(",")
+        with open("./data/out/transcript_ratio/genes_ratio_smaller_0.csv") as file:
+            smaller_genes = file.read().strip().split(",")
+        color_scatterplot_exonincorporation(data, out_dir, bigger_genes, smaller_genes)
+
+    # TODO Create boxplots
+    return None
 
 
 if __name__ == '__main__':
