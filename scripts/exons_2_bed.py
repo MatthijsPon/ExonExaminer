3fec63e118e9cb54718bcf148ad6dbdbf77466fd
Matthijs Pon <matthijs.pon@outlook.com>
2022-06-20 14:11:29 +0200

    forgot to add argument parser, now should work correctly

--- scripts/exons_2_bed.py
+++ scripts/exons_2_bed.py
@@ -4,29 +4,44 @@ Author: Matthijs Pon
 Description:
 """
 import pandas as pd
+import argparse as arg
 
 
-def write_to_bed(df, chrom, start, stop, id, out_file):
+def parse_arguments():
+    """Parse command line arguments
+
+    :return: parsed command line options
+    """
+    parser = arg.ArgumentParser(description="Convert exons to a bed file.")
+    parser.add_argument("pickle", type=str,
+                        help="Pandas pickle (saved gff3 dataframe)")
+    parser.add_argument("out_file", type=str,
+                        help="output filename")
+    args = parser.parse_args()
+    return args.pickle, args.out_file
+
+
+def write_to_bed(df, chrom, start, stop, item_id, out_file):
     """Write a gff3 dataframe to a bed file.
 
     :param df: pandas dataframe
     :param chrom: str/index, index to access chromosome information from df
     :param start: str/index, index to access start information from df
     :param stop: str/index, index to access stop information from df
-    :param id: str/index, index to access ID information from df
+    :param item_id: str/index, index to access ID information from df
     :param out_file: str, output file
     :return: None, file is created
     """
-    columns = [chrom, start, stop, id]
+    columns = [chrom, start, stop, item_id]
     df[columns].to_csv(out_file, sep="\t", index=False)
     return None
 
 
 def main():
     """Main function."""
-    in_file = "data/temp/parsed_human_gff.pickle"
-    out_file = "data/out/exons_2_bed/human_exons.bed"
-    df = pd.read_pickle(in_file)
+    pickle, out_file = parse_arguments()
+
+    df = pd.read_pickle(pickle)
     df = df.loc[df["type"] == "exon"]
     # BedTools sees the stop index as non-inclusive, but gff3 sees it as inclusive
     df["stop"] += 1
