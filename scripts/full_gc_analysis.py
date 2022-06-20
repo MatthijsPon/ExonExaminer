41450de0228e0313bc9532a24a8f511eabaf6b17
Matthijs Pon <matthijs.pon@outlook.com>
2022-06-20 16:52:50 +0200

    Init full_gc_analysis.py

---
+++ scripts/full_gc_analysis.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Author: Matthijs Pon
+Description:
+"""
+import argparse as arg
+
+
+def parse_arguments():
+    """Parse command line arguments
+
+    :return: parsed command line options
+    """
+    pass
+
+def main():
+    """Main function."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
