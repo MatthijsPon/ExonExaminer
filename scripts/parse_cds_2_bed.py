a75841718937387697a59c192641e41c2ee8c6e2
Matthijs Pon <matthijs.pon@outlook.com>
2022-06-27 10:02:29 +0200

    Added phase awareness to parse_cds_2_bed.py

--- scripts/parse_cds_2_bed.py
+++ scripts/parse_cds_2_bed.py
@@ -19,8 +19,9 @@ def parse_arguments():
                         help="gff3 file to parse")
     parser.add_argument("out_file", type=str,
                         help="output filename")
+    parser.add_argument("-p", "--phase", action="store_true")
     args = parser.parse_args()
-    return args.gff3, args.out_file
+    return args.gff3, args.out_file, args.phase
 
 
 def write_to_bed(df, chrom, start, stop, strand, parent, out_file):
@@ -41,10 +42,11 @@ def write_to_bed(df, chrom, start, stop, strand, parent, out_file):
     return None
 
 
-def parse_cds_from_gff(file):
+def parse_cds_from_gff(file, phase_aware=False):
     """Parse all the CDS regions from a gff3 file.
 
     :param file: iterable, open file object
+    :param phase_aware: bool, move the CDS start to the first codon in the exon (default: False)
     :return: pandas dataframe object, CDS information
     """
     data = {
@@ -57,9 +59,12 @@ def parse_cds_from_gff(file):
 
     for line in yield_gff_line(file):
         if line[2] == "CDS":
-
             data["chromosome"].append(line[0])
-            data["start"].append(line[3])
+            if phase_aware:
+                phase = int(line[7])
+                data["start"].append(int(line[3]) + phase)
+            else:
+                data["start"].append(line[3])
             data["stop"].append(line[4])
             data["strand"].append(line[6])
             data["parent"].append(determine_gff(line[8], "Parent=transcript:"))
@@ -69,10 +74,10 @@ def parse_cds_from_gff(file):
 
 def main():
     """Main function."""
-    gff, out_file = parse_arguments()
+    gff, out_file, phase = parse_arguments()
 
     with open(gff) as file:
-        df = parse_cds_from_gff(file)
+        df = parse_cds_from_gff(file, phase)
 
     # BedTools sees the stop index as non-inclusive, but gff3 sees it as inclusive
     df["start"] = df["start"].apply(pd.to_numeric)
