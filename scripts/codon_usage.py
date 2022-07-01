48e005ee91baf0eb85121a7d8d84491fd2abe9b1
Matthijs Pon <matthijs.pon@outlook.com>
2022-07-01 11:34:53 +0200

    Updated codon usage to calculate codon usage itself

--- scripts/codon_usage.py
+++ scripts/codon_usage.py
@@ -6,6 +6,7 @@ Description:
 import argparse as arg
 import pandas as pd
 from matplotlib import pyplot as plt
+from cds_split_fa_2_size_groups import yield_fasta_record
 
 
 def parse_arguments():
@@ -16,55 +17,14 @@ def parse_arguments():
     parser = arg.ArgumentParser(description="Determine codon usage of ")
     parser.add_argument("fasta", type=str,
                         help="fasta file with CDS sequences")
-    parser.add_argument("pickle", type=str,
-                        help="parsed gff pickle file")
     parser.add_argument("out_dir", type=str,
                         help="output directory (incl. trailing \\")
+    parser.add_argument("group_name", type=str,
+                        help="group name to use in filenames")
     args = parser.parse_args()
     if not args.out_dir.endswith("/") and not args.out_dir.endswith("\\"):
         raise ValueError("Please end the out_dir with a trailing \\ or /.")
-    return args.fasta, args.pickle, args.out_dir
-
-
-def parse_fasta(file, df):
-    """"""
-    seqs = {}
-    fa_id = ""
-    seq = []
-
-    for idx, line in enumerate(file):
-        line = line.strip()
-        if line.startswith("#"):
-            continue
-        elif not line:
-            continue
-        elif line.startswith(">"):
-            if fa_id and seq:
-                seqs[fa_id] = {
-                    "id": fa_id,
-                    "seq": "".join(seq)
-                }
-            line = line[1:].split(":")
-            start, stop = line[3][:-3].split("-")
-            start = str(int(start) + 1)
-            try:
-                fa_id = df.loc[(df["chromosome"] == line[2]) & (df["strand"] == line[3][-2]) &
-                               (df["start"] <= start) & (df["stop"] >= stop), "id"].iloc[0]
-            except IndexError:
-                fa_id = ""
-            if type(fa_id) != str:
-                print(fa_id)
-                raise ValueError("Multiple exons map to the same location.")
-            seq = []
-        else:
-            seq.append(line)
-
-    if fa_id and seq:
-        seqs[fa_id] = {
-            "id": fa_id,
-            "seq": "".join(seq)
-        }
-    return seqs
+    return args.fasta, args.out_dir, args.group_name
 
 
 def codon_aa_table():
@@ -79,11 +39,11 @@ def codon_aa_table():
     return codon_table
 
 
-def determine_codon_usage(sequences):
+def determine_codon_usage(file):
     """"""
     codons = {}
 
-    for fa_id, fasta in sequences.items():
+    for fa_id, fasta in yield_fasta_record(file):
         len_seq = len(fasta["seq"])
         for i in range(0, len_seq, 3):
             if i + 3 > len_seq:
@@ -125,23 +85,13 @@ def hbar_codon_usage(df, filename):
 
 def main():
     """"""
-    fasta, pickle, out_dir = parse_arguments()
-    df = pd.read_pickle(pickle)
-    # parse sequences
+    fasta, out_dir, group = parse_arguments()
+
+
     with open(fasta) as file:
-        seqs = parse_fasta(file, df)
-
-    # divide into groups
-    groups = {"size_<50": {key: seqs[key] for key in seqs.keys() & list(df.loc[df["size"] < 50, "id"])},
-              "size_50-300": {key: seqs[key] for key in seqs.keys() &
-                              list(df.loc[(df["size"] >= 50) & (df["size"] <= 300), "id"])},
-              "size_300>": {key: seqs[key] for key in seqs.keys() & list(df.loc[df["size"] > 300, "id"])}}
-
-    # determine codon usage per group
-    data = {}
-    for idx, group in groups.items():
-        codon_usage = codon_usage_df(group)
-        hbar_codon_usage(codon_usage, "{}hbar_graph_{}.png".format(out_dir, idx))
+        cu_dict = determine_codon_usage(file)
+    cu_df = codon_usage_df(cu_dict)
+    hbar_codon_usage(cu_df, "{}hbar_graph_{}.png".format(out_dir, group))
 
     return None
 
