9db69962c41e799670dfafe4adab1827f593e301
MatthijsPon <matthijs.pon@outlook.com>
2022-07-06 14:24:10 +0200

    sbatch script for when a job fails and need re-running

---
+++ incomplete_slurm_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=exonexaminer
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=m.n.pon@lumc.nl
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=12gb
+#SBATCH --time=4-00:00:00
+#SBATCH --output=logs/exon_examiner_%j.log
+
+module load tools/miniconda/python3.8/4.9.2
+module load genomics/ngs/bedtools2/2.29.1
+conda activate exonexaminer
+
+snakemake -j4 --rerun-incomplete
+
